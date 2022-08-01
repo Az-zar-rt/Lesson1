79106927cd2ce78a0da23c82ec57ca21cb027e78
Az-zar-rt <beloshapka2001.09.03@gmail.com>
2022-08-01 18:03:52 +0300

    commitsecond

--- Lesson1/Program.cs
+++ Lesson1/Program.cs
@@ -1,5 +1,7 @@
 ﻿
 Console.WriteLine("Hello, World!");
 Console.WriteLine("How are you?");
-Console.WriteLine("I am good?");
+Console.WriteLine("I am good!");
 Console.WriteLine("And you?");
+Console.WriteLine("I'm fine");
+Console.WriteLine("Nice!");
